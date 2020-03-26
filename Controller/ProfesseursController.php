f6b751e5f07b0ff36f87479f357b0b1f38b12f24
Daouda <kontedaouda96@gmailcom>
2020-03-26 18:12:20 +0000

    Ajout des méthodes __construc et getListProfesseurs et gestion des requets dans le controlleur Professeur

---
+++ Controller/ProfesseursController.php
@@ -0,0 +1,23 @@
+<?php
+    require 'Model/ProfesseursModel.php';
+    
+    class ProfesseursController{
+
+        public function __construct(){
+            if (isset($_GET['action'])){
+                $action = $_GET['action'];
+                if ($action === 'consulter'){
+                    if (isset($_GET['id'])){
+                        $id = intval($_GET['id']);
+                        $professeurs = new Professeurs($id);
+                        require 'View/PofesseursInfos.php';
+    
+                    }else{
+                        $professeurs = Professeurs::getListProfesseurs();
+                        require 'View/ProfesseursList.php';
+                    }
+    
+                }
+        }
+    }
+}
--- Model/ProfesseursModel.php
+++ Model/ProfesseursModel.php
@@ -7,7 +7,30 @@
         public $matiere;
 
         public function __construct($id){
+            if(!isset($bdd)){
+                require 'Model/db.php';
+            }
+            $retour = $bdd->prepare('SELECT * from professeurs WHERE id = ?');
+            $retour->execute(array($id));
+            $professeurs = $retour->fetch();
 
+            $this->id = $professeurs['id'];
+            $this->prenom = $professeurs['prenom'];
+            $this->nom = $professeurs['nom'];
+            $this->age = $professeurs['age'];
+            $this->matiere = $professeurs['matiere'];
+        }
+        public static function getListProfesseurs(){
+            if(!isset($bdd)){
+                require 'Model/db.php';
+            }
+            $reponse = $bdd->query('SELECT * from professeurs');
+            $donnees = array();
+            while ($prof = $reponse->fetch()){
+                $professeurs = new Professeurs($prof['id']);
+                $donnees[] = $professeurs;
+            }
+            return $donnees;
         }
 
         public function modif($prenom, $nom, $age, $matiere){
