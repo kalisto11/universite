37da1a7d24a9969cf8f3caf8305af3703c4f13e9
Daouda <kontedaouda96@gmailcom>
2020-03-26 20:30:24 +0000

    Traitement des requetes d'ajout, de modification et de suppression dans le fichier ProfesseursController.php

--- Controller/ProfesseursController.php
+++ Controller/ProfesseursController.php
@@ -9,15 +9,52 @@
                 if ($action === 'consulter'){
                     if (isset($_GET['id'])){
                         $id = intval($_GET['id']);
-                        $professeurs = new Professeurs($id);
+                        $professeur = new Professeurs($id);
                         require 'View/PofesseursInfos.php';
     
                     }else{
-                        $professeurs = Professeurs::getListProfesseurs();
+                        $professeur = Professeurs::getListProfesseurs();
                         require 'View/ProfesseursList.php';
                     }
-    
                 }
+                elseif ($action === 'supprimer'){
+                    if (isset($_GET['id'])){
+                        $id = intval($_GET['id']);
+                        $professeur = new Professeurs($id);
+                        $professeur->supprimer();
+                        $professeur = Professeurs::getListProfesseurs();
+                        require 'View/ProfesseursList.php';
+                    }
+                }
+                elseif($action === 'modifier'){
+                    $id = ($_GET['id']);
+                    $professeur = new Professeurs($id);
+                    require 'View/ProfesseursModifForm.php';
+                }
+                elseif($action === 'ajouter'){
+                    require 'View/ProfesseursAjoutForm.php';
+                }
+                else{
+                    
+                }
+            }
+            else if (isset($_POST['action'])){
+                $action = $_POST['action'];
+                switch($action){
+                    case 'modifier':
+                        $id = intval($_POST['id']);
+                        $professeur = new Professeurs($id);
+                        $professeur->modif($_POST['prenom'], $_POST['nom'], $_POST['age'], $_POST['matiere']);
+                        require 'View/ProfesseursInfos.php';
+                    break;
+                    case 'ajouter':
+                        Professeurs::ajoutProfesseurs($_POST['prenom'], $_POST['nom'], $_POST['age'], $_POST['matiere']);
+                        $professeur = Professeurs::getListProfesseurs();
+                        require 'View/ProfesseursList.php';
+                    break;
+                    default:
+                }
+            }
+            else{}
         }
     }
-}
